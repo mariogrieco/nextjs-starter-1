2fd79b7ac7dfa8b775c6502a4e7c6aaa567f1a9a
Max Li <maxlivinci@gmail.com>
2018-08-06 15:13:17 +0800

    update server route/index

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,13 @@
-const rootRouter = require("./root");
-const business = require("./business");
-const blog = require("./blog");
+const normalizedPath = require("path").join(__dirname);
+const getFileName = file => file.replace(/\.[^/.]+$/, "");
 
 module.exports = server => {
-  server.use("/", rootRouter);
-  server.use("/business", business);
-  server.use("/blog", blog);
+  server.use("/", require("./root"));
+  require("fs")
+    .readdirSync(normalizedPath)
+    .forEach(file => {
+      if (file !== "index.js" && file !== "root.js") {
+        server.use(`/${getFileName(file)}`, require(`./${file}`));
+      }
+    });
 };
